edf0ac5d686e557bce199429a8d1a91db3d3a53b
Tamanna Garva <155551840+Tamanna005@users.noreply.github.com>
2024-02-06 15:20:46 +0530

    Add files via upload

---
+++ object.java
@@ -0,0 +1,29 @@
+class Pen { 
+    String color; //Properties
+    String type;
+    
+public void Write(){ //Method
+        System.out.println("Let's make cool code!");
+    }
+}
+ class OOPS{ // Main class
+    public static void main(String [] args){
+        Pen Pen1 = new Pen(); // Object
+        Pen1.color = "blue"; // Assigning values
+        Pen1.type = "Bollpen";
+
+            Pen1.Write();
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
